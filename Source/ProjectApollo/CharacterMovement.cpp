1d1025e27b799790817b6aef11f157ed64d915b4
brysonckennedy <brysonckennedy@gmail.com>
2022-10-01 16:37:53 -0500

    Added Basic Character Movement
    I made changes to CharacterMovement.cpp to add basic movement. I also added a new gamemode and a mesh to use on character.

--- Source/ProjectApollo/CharacterMovement.cpp
+++ Source/ProjectApollo/CharacterMovement.cpp
@@ -9,6 +9,26 @@ ACharacterMovement::ACharacterMovement()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	GetCharacterMovement()->JumpZVelocity = 500.0f;
+	GetCharacterMovement()->AirControl = 2.0f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+
+	FollowCamera->bUsePawnControlRotation = false;
+
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +50,34 @@ void ACharacterMovement::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACharacterMovement::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACharacterMovement::MoveRight);
 }
 
+void ACharacterMovement::MoveForward(float Axis)
+{
+
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(Direction, Axis);
+	
+}
+
+void ACharacterMovement::MoveRight(float Axis)
+{
+	
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(Direction, Axis);
+
+}
--- Source/ProjectApollo/CharacterMovement.h
+++ Source/ProjectApollo/CharacterMovement.h
@@ -4,6 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "Components/InputComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 #include "CharacterMovement.generated.h"
 
 UCLASS()
@@ -14,6 +21,14 @@ class PROJECTAPOLLO_API ACharacterMovement : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ACharacterMovement();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		UCameraComponent* FollowCamera;
+
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
 
 protected:
 	// Called when the game starts or when spawned
